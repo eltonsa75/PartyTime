e7f37b6e147a6dc9ab05f6b85a19e6d5e182b9ee
Elton Andrade <eltonsa75@hotmail.com>
2025-04-27 23:09:43 -0300

    Criando o CSS da página Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 
+// Components
+import Navbar from './components/Navbar'
+
 function App() {
 
   return (
    <div className='App'>
-    <h1>Party Time</h1>
+    <Navbar />
     <Outlet />
    </div>
   )
--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import partyFetch from "../axios/config";
+
+import { useState, useEffect } from "react";
+
+import { Link } from "react-router-dom";
+
+import "./Home.css";
 
 const Home = () => {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  )
-}
+  const [parties, setParties] = useState(null);
 
-export default Home
+  // Load parties
+  useEffect(() => {
+    const loadParties = async () => {
+      const res = await partyFetch.get("/parties");
+
+      setParties(res.data);
+    };
+
+    loadParties();
+  }, []);
+
+  if (!parties) return <p>Carregando...</p>;
+
+  return (
+    <div className="home">
+      <h1>Suas Festas</h1>
+      <div className="parties-container">
+        {parties.length === 0 && <p>Não há festas criadas!</p>}
+        {parties.map((party) => (
+          <div className="party" key={party._id}>
+            <img src={party.image} alt={party.title} />
+            <h3>{party.title}</h3>
+            <Link to={`/party/${party._id}`} className="btn-secondary">
+              Detalhes
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
