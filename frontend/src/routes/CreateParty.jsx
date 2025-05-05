0c850caa085a70a00ce25ac9eb14d51c5e93a8bc
Elton Andrade <eltonsa75@hotmail.com>
2025-05-04 21:43:59 -0300

    Criando o objeto do formulário

--- frontend/src/routes/CreateParty.jsx
+++ frontend/src/routes/CreateParty.jsx
@@ -1,47 +1,94 @@
-import partyFetch from '../axios/config'
+import partyFetch from "../axios/config";
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import { useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
-import "./Form.css"
+import "./Form.css";
 
 const CreateParty = () => {
-
-  const [services, setServices] = useState([])
+  const [services, setServices] = useState([]);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [description, setDescription] = useState("");
+  const [budget, setBudget] = useState("");
+  const [image, setImage] = useState("");
+  const [partyServices, setPartyServices] = useState([]);
 
   // Load services
   useEffect(() => {
-
     const loadServices = async () => {
-      const res = await partyFetch.get("/services")
-
-      setServices(res.data)
-    }
+      const res = await partyFetch.get("/services");
+      setServices(res.data);
+    };
 
     loadServices();
+  }, []);
 
-  }, [])
+  // add or remove services
+  const handleServices = (e) => {
+    const checked = e.target.checked;
+    const value = e.target.value;
 
+    const filteredService = services.filter((s) => s._id === value);
+
+    console.log(filteredService)
+
+    if(checked) {
+      setPartyServices((services) => [...services, filteredService[0]])
+    } else {
+      setPartyServices((services) => services.filter((s) => s._id !== value))
+    }
+
+    console.log(partyServices);
+  };
+
+  const createParty = (e) => {
+    e.preventDefault();
+
+    const party = {
+      title,
+      author,
+      description,
+      budget,
+      image,
+      services: partyServices,
+    };
+    console.log(party)
+  };
 
   return (
     <div className="form-page">
       <h2>Crie sua próxima Festa</h2>
       <p>Defina o seu orçamento e escolha os serviços</p>
-      <form>
+      <form onSubmit={(e) => createParty(e)}>
         <label>
           <span>Nome da festa:</span>
-          <input type="text" placeholder="Seja criativo..." required />
+          <input
+            type="text"
+            placeholder="Seja criativo..."
+            required
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
         </label>
         <label>
           <span>Anfitrião:</span>
-          <input type="text" placeholder="Quem está dando a festa?" required />
+          <input
+            type="text"
+            placeholder="Quem está dando a festa?"
+            required
+            onChange={(e) => setAuthor(e.target.value)}
+            value={author}
+          />
         </label>
         <label>
           <span>Descrição:</span>
           <textarea
             placeholder="Conte mais sobre a festa..."
             required
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
           ></textarea>
         </label>
         <label>
@@ -50,6 +97,8 @@ const CreateParty = () => {
             type="number"
             placeholder="Quando você pretende investir?"
             required
+            onChange={(e) => setBudget(e.target.value)}
+            value={budget}
           />
         </label>
         <label>
@@ -58,23 +107,30 @@ const CreateParty = () => {
             type="text"
             placeholder="Insira a url de uma imagem"
             required
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
           />
         </label>
         <div>
           <h2>Escolha os serviços</h2>
           <div className="services-container">
             {services.length === 0 && <p>Carregando...</p>}
-            {services.length > 0 && services.map((service) => (
-              <div className='service' key={service._id}>
-                <img src={service.image} alt={service.name} />
-                <p className="service-name">{service.name}</p> 
-                <p className="service-price">R${service.price}</p>
-                <div className="checkbox-container">
-                  <input type='checkbox' value={service._id} />
-                  <p>Marque para solicitar</p>
+            {services.length > 0 &&
+              services.map((service) => (
+                <div className="service" key={service._id}>
+                  <img src={service.image} alt={service.name} />
+                  <p className="service-name">{service.name}</p>
+                  <p className="service-price">R${service.price}</p>
+                  <div className="checkbox-container">
+                    <input
+                      type="checkbox"
+                      value={service._id}
+                      onChange={(e) => handleServices(e)}
+                    />
+                    <p>Marque para solicitar</p>
+                  </div>
                 </div>
-                </div>
-            ))}
+              ))}
           </div>
         </div>
         <input type="submit" value="Criar Festa" className="btn" />
